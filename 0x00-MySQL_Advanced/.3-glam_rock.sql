d402a4bc89bb597ec29d2128845b8db69ef22837
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-10 00:07:29 +0100

    Renew

--- 0x00-MySQL_Advanced/.3-glam_rock.sql
+++
@@ -1,5 +0,0 @@
--- SQL script for Listing all bands with Glam rock as their main style, ranked by their longevity.
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- script that lists all bands with Glam rock as their
+-- main style, ranked by their longevity
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM 
+metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
