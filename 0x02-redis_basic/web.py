436c4772554505a7b4bdd363f3d5e625a4345bf1
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-16 00:12:11 +0100

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
-import requests
-from functools import wraps
+"""
+Implementing a web cache
+"""
+
+from time import sleep
 from typing import Callable
+import redis
+from functools import wraps
+import requests
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def url_count(method: Callable) -> Callable:
+    """Wrapper function to count frequency of url"""
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def count_wrapper(*args):
+        """Callback function to be returned"""
+        cache = redis.Redis()
+        key = "count:" + args[0]
+        cache.incrby(key, 1)
+        cache.expire(key, 10)
+        return method
+    return count_wrapper
 
 
-@data_cacher
+@url_count
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """Web cache and tracker"""
+    res = requests.get(url)
+    return res
